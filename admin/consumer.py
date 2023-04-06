69a85a399f40e0acef44b1f01c9174bb88d1f3c4
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-04-06 12:16:55 +0530

    rabbit mq is working fine now

--- admin/consumer.py
+++ admin/consumer.py
@@ -1,12 +1,14 @@
 import pika
+import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
 # django.setup()
 
 # from products.models import Product
 
-params = pika.URLParameters(
-    'amqps://ivzdrafs:ayWp8Y9LV_TUCuHc5c_g7zQXiDJITB0t@kebnekaise.lmq.cloudamqp.com/ivzdrafs')
+params = pika.URLParameters(os.environ['AMQPS_KEY'])
 
 connection = pika.BlockingConnection(params)
 
@@ -15,7 +17,7 @@ channel = connection.channel()
 channel.queue_declare(queue='admin')
 
 
-def callback(ch, method, properties, body) :
+def callback(ch, method, properties, body):
     print('Received in admin')
     print(body)
     # id = json.loads(body)
@@ -26,7 +28,8 @@ def callback(ch, method, properties, body) :
     # print('Product likes increased!')
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='admin', on_message_callback=callback,
+                      auto_ack=True)
 
 print('Started Consuming')
 
--- admin/products/producer.py
+++ admin/products/producer.py
@@ -2,7 +2,8 @@ import json
 import pika
 
 params = pika.URLParameters(
-    'amqps://ivzdrafs:ayWp8Y9LV_TUCuHc5c_g7zQXiDJITB0t@kebnekaise.lmq.cloudamqp.com/ivzdrafs')
+    'amqps://ivzdrafs:ayWp8Y9LV_TUCuHc5c_g7zQXiDJITB0t@kebnekaise.lmq'
+    '.cloudamqp.com/ivzdrafs')
 
 connection = pika.BlockingConnection(params)
 
@@ -11,5 +12,4 @@ channel = connection.channel()
 
 def publish(method, body):
     properties = pika.BasicProperties(method)
-    # properties = pika.BasicProperties(method)
     channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
